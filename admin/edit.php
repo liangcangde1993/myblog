a2dcfb1bcd79b68c9e8ef6cb8b5c31c608db7d2e
liangcangde1993 <liangcangde19932163.com>
2017-02-21 11:16:21 +0800

    review edit

--- admin/edit.php
+++ admin/edit.php
@@ -1,37 +1,49 @@
 <?php
-      /*  session_start();
+        session_start();
         if (!isset($_SESSION['pwd'])){
             $url="./login.html";
             echo "<script language=\"javascript\">";
             echo "location.href=\"$url\"";
             echo "</script>";
-        }*/
-        $title = $_POST['title'];
-        $content = $_POST['content'];
-        $own = $_POST['own'];
-        $create_time = $_POST['create_time'];
-        $link = $_POST['link'];
-        try {
-            $dbname="root";
-            $dbpass="123456";
-            $dbhost="127.0.0.1";
-            $dbdatabase="blog";
-            $db_connect= new mysqli($dbhost,$dbname,$dbpass,$dbdatabase);
+            exit;
+        }
+	$requiredKeys = array('title', 'content','category','link');
+	         foreach ($requiredKeys as $key) {
+	        if (!isset($_POST[$key])) {
+	            throw new InvalidArgumentException("missing required key $key");
+	        }
+	    }
 
-            $strsql="INSERT INTO `article` (`title`,`content`,`own`,`create_time`,`link`) VALUES ('$title','$content','$own','$create_time','$link')";//var_dump($strsql);die();
-            $result=$db_connect->query($strsql);
-            $row= mysqli_affected_rows($db_connect);//var_dump($row);die();
-            // $result->close();
-            $db_connect->close();
+	        $title = $_POST['title'];
+	        $content = $_POST['content'];
+	        $category = $_POST['category'];
+	        $create_time = time();
+	        $link = $_POST['link'];
 
-            if($row){
-                echo true;
-            }
-            else{
-               echo false;
-            } 
+	            $dbname="root";
+	            $dbpass="123456";
+	            $dbhost="127.0.0.1";
+	            $dbdatabase="blog";
+	            $db_connect= new mysqli($dbhost,$dbname,$dbpass,$dbdatabase);
+	    	 $db_connect->set_charset('utf8');
 
-        	}
-        	catch (Exception $e){}
+	            $strsql="INSERT INTO `article` (`title`,`content`,`own`,`create_time`,`link`) VALUES (?,?,?,?,?)";
+	            if (!($stmt = $db_connect->prepare($strsql))) {
+	    echo "Prepare failed: (" . $db_connect->errno . ") " . $db_connect->error;
+	    }
+	    $titel = $title;
+	    $content = $content;
+	    $category =  $category;
+	    $create_time = $create_time;
+	    $link = $link;
+	    if (!$stmt->bind_param("sssss", $title,$content,$category,$create_time,$link)) {
+	    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+	    if (!$stmt->execute()) {
+	    echo  false;
+	}
+
+	       echo true;
+
 ?>
 
