4e087a144db92ae6bc0b0f605bf57c5e240b9b49
liangcangde1993 <liangcangde19932163.com>
2017-02-21 12:19:02 +0800

    review exchange updata

--- admin/exchange.php
+++ admin/exchange.php
@@ -1,27 +1,55 @@
 <?php
+
 session_start();
 if (!isset($_SESSION['pwd'])){
 	$url="./login.html";
 	echo "<script language=\"javascript\">";
 	echo "location.href=\"$url\"";
 	echo "</script>";
+	exit;
 }
-$id = $_GET['id'];
-try {
+
+	if (!isset($_GET['id'])) {
+	        header("Location: ./index.php"); 
+	        exit;
+	    }
+	$id = $_GET['id'];
+
     $dbname="root";
     $dbpass="123456";
     $dbhost="127.0.0.1";
     $dbdatabase="blog";
     $db_connect= new mysqli($dbhost,$dbname,$dbpass,$dbdatabase);
+    $db_connect->set_charset('utf8');
 
-    $strsql="select * from `article` where id =".$id;
-    $result=$db_connect->query($strsql);
-    $row= mysqli_fetch_assoc($result);//var_dump($row);die();
-    $data[] = $row;
+    $strsql="select `id`,`title`,`link`,`content`,`own` from `article` where id =?";
+   if (!($stmt = $db_connect->prepare($strsql))) {
+    echo "Prepare failed: (" . $db_connect->errno . ") " . $db_connect->error;
+    }
+    $val = $id;
+    if (!$stmt->bind_param("s", $val)) {
+    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+}
+
+    $stmt->bind_result($id,$title,$link,$content,$own);
+    if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+}
+
+    	$data = array();
+	    while ($stmt->fetch())
+	    {
+	    	$data ['id']= $id;
+	           $data['title']= $title;
+		$data ['link']= $link;
+		$data ['content']= $content;
+		$data ['own']= $own;
+	    }
+	    
+	    $stmt->close();
 	    $db_connect->close();
 	 
-	}
-	catch (Exception $e){}
+	
 ?>
 
 
@@ -44,11 +72,10 @@ try {
 
 <div style="text-align: left;margin-top: 50px">
 
-	<?php  foreach ($data as $k => $v) {     ?>
 
 <div>
-<select id="select" style="width: 30%;height: 30px"><?php echo $v['own']; ?>
-<option value='own' selected="">own</option> 
+<select id="select" style="width: 30%;height: 30px"><?php echo $data['own']; ?>
+<option value='category' selected="">category</option> 
 <option value='life'>life</option> 
 <option value='work'>work</option> 
 <option value='play'>play</option> 
@@ -58,54 +85,62 @@ try {
 <div style="margin-top: 20px">
 <p><label>title:</label></p>
 
-<input id="title"  type="text" name="title" value="<?php echo $v['title']; ?>" style="height: 40px;width: 60%" placeholder="title" >    
+<input id="title"  type="text" name="title" value="<?php echo $data['title']; ?>" style="height: 40px;width: 60%" placeholder="title" >    
 </div>
 <div  style="margin-top: 20px">
 <p><label>link:</label></p>
 
-<input  type="text" id="link" name="link" value="<?php echo $v['link']; ?>" style="height: 40px;width: 60%"  placeholder="link"  onblur="check()" >    
+<input  type="text" id="link" name="link" value="<?php echo $data['link']; ?>" style="height: 40px;width: 60%"  placeholder="link"  onblur="check()" >    
 
 </div>
 <div style="margin-top: 20px">
-<textarea id="text" style="width: 80%;height: 400px;overflow-y:auto" placeholder="article"><?php echo $v['content']; ?></textarea>
+<textarea id="text" style="width: 80%;height: 400px;overflow-y:auto" placeholder="article"  onblur="check_len()"><?php echo $data['content']; ?></textarea>
 </div>
 
 <div id="sub" style="margin-top: 30px">
-    <button value="" id="btn" style="width: 80px;height: 40px;border-radius: 10px" onclick="edit('<?php echo $v['id']; ?>')">OK</button>
+    <button value="" id="btn" style="width: 80px;height: 40px;border-radius: 10px" onclick="edit(<?php echo $data['id']; ?>)">OK</button>
 </div>
-<?php
-}
-?>
+
 </div>
 
 <script type="text/javascript">
 
-            window.onload=function(){ 
+		            window.onload=function(){ 
 
-	  var link = document.getElementById("link").value;
-            var text = document.getElementById("text");
-            if(link != ''){
-                text.readOnly=true;
-                text.placeholder="deny write";
-            }else{
-                text.readOnly=false;
-                text.placeholder="article";
-            }
+			  var link = document.getElementById("link").value;
+		            var text = document.getElementById("text");
+		            if(link != ''){
+		                text.readOnly=true;
+		                text.placeholder="deny write";
+		            }else{
+		                text.readOnly=false;
+		                text.placeholder="article";
+		            }
 
-	}
+			}
 
-	 function check(){
-            var link = document.getElementById("link").value;
-            var text = document.getElementById("text");
-            if(link != ''){
-                text.readOnly=true;
-                text.placeholder="deny write";
-            }else{
-                text.readOnly=false;
-                text.placeholder="article";
-            }
+			 function check(){
+		            var link = document.getElementById("link").value;
+		            var text = document.getElementById("text");
+		            if(link != ''){
+		                text.readOnly=true;
+		                text.placeholder="deny write";
+		            }else{
+		                text.readOnly=false;
+		                text.placeholder="article";
+		            }
 
-        }
+		        }
+
+		         function check_len(){  
+		            var text = document.getElementById("text").value;
+		            var str = text.length;   
+		            if(str>=100){
+		              alert("max is 100");
+		            }
+		            }
+
+        
 	
 
 			function edit(id){
@@ -113,8 +148,8 @@ try {
 			var title = document.getElementById("title").value;
 			var text = document.getElementById("text").value;
 			var link = document.getElementById("link").value;
-			var timestamp=new Date().getTime();
-			var postData = {"id":id,"title":title,"text":text,"own":select,"last_ex_time":timestamp,"link":$link}, 
+			
+			var postData = {"id":id,"title":title,"text":text,"category":select,"link":link}, 
 			postData = (function(obj){ 
 		    var str = "";
 		    for(var prop in obj){
@@ -122,7 +157,7 @@ try {
 		    }
 		    str=str.substring(0,str.length-1);
 		    return str;
-			})(postData);//alert(postData);return false;
+			})(postData);
 			var xhr = new XMLHttpRequest();
 			xhr.open("POST", "./updata.php", true);
 			xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
@@ -130,15 +165,14 @@ try {
 			    var XMLHttpReq = xhr;
 			    if (XMLHttpReq.readyState == 4 && XMLHttpReq.status == 200) {
 			        
-			             var text = XMLHttpReq.responseText;//console.log(text);return false;
-			    // alert(text);
+			             var text = XMLHttpReq.responseText;
 			         	if(text == true){
 			         		alert('exchange file success!');
 			         		window.location.href="./index.php"; 
 			         	}
 			         	else{
 			         		alert('exchange file error!');
-		         			location.reload();
+		         			//location.reload();
 
 			         	}
 			            
--- admin/updata.php
+++ admin/updata.php
@@ -1,38 +1,54 @@
 <?php
+
         session_start();
         if (!isset($_SESSION['pwd'])){
             $url="./login.html";
             echo "<script language=\"javascript\">";
             echo "location.href=\"$url\"";
             echo "</script>";
+            exit;
         }
+   
+
+        	$requiredKeys = array('id','title', 'text','category','link');
+	         foreach ($requiredKeys as $key) {
+	        if (!isset($_POST[$key])) {
+	            throw new InvalidArgumentException("missing required key $key");
+	        }
+	    }
         $id = $_POST['id'];
         $title = $_POST['title'];
         $content = $_POST['text'];
-        $own = $_POST['own'];
-        $last_ex_time = $_POST['last_ex_time'];
+        $category = $_POST['category'];
         $link = $_POST['link'];
-        try {
-            $dbname="root";
+        $last_ex_time = time();
+           $subject = trim($_POST['text']);
+	      $len     = mb_strlen($subject, 'UTF-8');
+	    if ( $len > 100) {
+	        throw new InvalidArgumentException('subject required and maxlength is 100');
+	    }
+               $dbname="root";
             $dbpass="123456";
             $dbhost="127.0.0.1";
             $dbdatabase="blog";
             $db_connect= new mysqli($dbhost,$dbname,$dbpass,$dbdatabase);
+	   $db_connect->set_charset('utf8');
 
-            $strsql="UPDATE `article` SET `title` = '$title',`content`='$content',`own`='$own',`last_ex_time`='$last_ex_time' ,`link`='$link'  WHERE `id` = ".$id;
-            $result=$db_connect->query($strsql);
-            $row= mysqli_affected_rows($db_connect);//var_dump($row);die();
-            // $result->close();
-            $db_connect->close();
+            $strsql="UPDATE `article` SET `title` = '$title',`content`='$content',`own`='$category',`last_ex_time`='$last_ex_time' ,`link`='$link'  WHERE `id` = ?";
+              if (!($stmt = $db_connect->prepare($strsql))) {
+	    echo "Prepare failed: (" . $db_connect->errno . ") " . $db_connect->error;
+	    }
+	    $val = $id;
+	    if (!$stmt->bind_param("s", $val)) {
+	    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+	    if (!$stmt->execute()) {
+	    	echo false;
+	}   
+	            echo  true; 
+	   $stmt->close();
+		$db_connect->close();
 
-            if($row){
-                echo true;
-            }
-            else{
-               echo false;
-            } 
 
-        	}
-        	catch (Exception $e){}
 ?>
 
