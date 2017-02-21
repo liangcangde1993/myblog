a56fd3a1b7cfdc1c7c9d677c68043adbfaf78a06
liangcangde1993 <liangcangde19932163.com>
2017-02-21 15:02:18 +0800

    add page.php

--- blog/page.php
+++ blog/page.php
@@ -1,20 +1,60 @@
 <?php
-$id = $_GET['id'];
-try {
+	if(!isset($_GET['id'])){
+		header("Location: ./index.php"); 
+		      exit;
+			}
+	$id = $_GET['id'];
+
     $dbname="root";
     $dbpass="123456";
     $dbhost="127.0.0.1";
     $dbdatabase="blog";
     $db_connect= new mysqli($dbhost,$dbname,$dbpass,$dbdatabase);
+     $db_connect->set_charset('utf8');
 
-    $strsql="select * from `article` where id =".$id;
-    $result=$db_connect->query($strsql);
-    $row= mysqli_fetch_assoc($result);//var_dump($row);die();
-    $data[] = $row;
-	    $db_connect->close();
-	 
+       $strsql="SELECT `name` FROM `category` ";
+  if ($stmt = $db_connect->prepare($strsql))
+	{
+	      if (!$stmt->execute()) {
+				    	echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+				}  
+	    $stmt->store_result();
+	    $row = $stmt->num_rows;
+	    $stmt->bind_result($name);
+	    $cate = array();
+	    while ($stmt->fetch())
+	    {
+	    	$cate []= $name;
+	       
+	    }
+	    	$stmt->close();
 	}
-	catch (Exception $e){}
+
+
+    $strsql2="SELECT `title`,`content` FROM `article` WHERE id =?";
+         if (!($stmt2 = $db_connect->prepare($strsql2))) {
+	    echo "Prepare failed: (" . $db_connect->errno . ") " . $db_connect->error;
+	    }
+	    $val = $id;
+	    if (!$stmt2->bind_param("s", $val)) {
+	    echo "Binding parameters failed: (" . $stmt2->errno . ") " . $stmt2->error;
+	}
+	    if (!$stmt2->execute()) {
+	    	echo "Execute failed: (" . $stmt2->errno . ") " . $stmt2->error;
+	}   
+	        $stmt2->bind_result($title,$content);
+	    $data = array();
+	    while ($stmt2->fetch())
+	    {
+	    	$data ['title']= $title;
+	    	$data ['content']= $content;
+	       
+	    }
+	   $stmt2->close();
+
+		$db_connect->close();
+	 
+	
 ?>
 
 
@@ -26,19 +66,10 @@ try {
      <tr>
         <td><p><h1>OurBlog</h1></p></td>
         <td style="width: 50px"></td>
-        <td><a href="./index.php" style="text-decoration:none; "><p>HEAD</p></a></td>
+        <?php  for($i=0;$i<$row;$i++) { ?>
+        <td><a href="./data.php?cate=<?php echo $cate[$i]; ?>" style="text-decoration:none; "><p><?php   echo  $cate[$i]; ?></p></a></td>
         <td style="width: 50px"></td>
-        <td><a href="./data.php?p=linux" style="text-decoration:none; "><p>LINUX</p></a></td>
-        <td style="width: 50px"></td>
-        <td><a href="./data.php?p=apache" style="text-decoration:none; "><p>APACHE</p></a></td>
-        <td style="width: 50px"></td>
-        <td><a href="./data.php?p=php" style="text-decoration:none; "><p>PHP</p></a></td>
-        <td style="width: 50px"></td>
-        <td><a href="./data.php?p=mysql" style="text-decoration:none; "><p>MYSQL</p></a></td>
-        <td style="width: 50px"></td>
-        <td><a href="./data.php?p=js" style="text-decoration:none; "><p>JS</p></a></td>
-        <td style="width: 50px"></td>
-        <td><a href="./data.php?p=misc" style="text-decoration:none; "><p>MISC</p></a></td>
+      <?php } ?>
     </tr>
 
 </table>
@@ -47,23 +78,20 @@ try {
 
 <div style="text-align: left;margin-top: 50px">
 
-	<?php  foreach ($data as $k => $v) {     ?>
 
 
 
 <div style="margin-top: 20px">
 
-<h1><p><?php echo $v['title']; ?></p> </h1> 
+<h1><p><?php echo $data['title']; ?></p> </h1> 
 </div>
 
 <div style="margin-top: 30px">
-<span><?php echo $v['content']; ?></span>
+<span><?php echo $data['content']; ?></span>
 </div>
 
 
-<?php
-}
-?>
+
 </div>
 
 
