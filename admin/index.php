944f189982a8858ee4294d84b050b7acacd69a89
liangcangde1993 <liangcangde19932163.com>
2017-02-23 09:27:59 +0800

    ./admin/index ok

--- admin/index.php
+++ admin/index.php
@@ -1,40 +1,25 @@
 <?php
-
-session_start();
-if (!isset($_SESSION['pwd'])){
-	$url="./login.html";
-	echo "<script language=\"javascript\">";
-	echo "location.href=\"$url\"";
-	echo "</script>";
+	session_start();
+	if (!isset($_SESSION['userid'])){
+	header("Location: ./login.php"); 
 	exit;
-}
-
-    $dbname="root";
-    $dbpass="123456";
-    $dbhost="127.0.0.1";
-    $dbdatabase="blog";
-    $db_connect= new mysqli($dbhost,$dbname,$dbpass,$dbdatabase);
-    $db_connect->set_charset('utf8');
-
-    $strsql="SELECT `id`, `title`, `link` FROM `article` ORDER BY `create_time` DESC";
-   if ($stmt = $db_connect->prepare($strsql))
-	{
-	    $stmt->execute();
-	    $stmt->store_result();
-	    $row = $stmt->num_rows;
-	    $stmt->bind_result($id, $title, $link);
-	    $data = array();
-	    while ($stmt->fetch())
-	    {
-	    	$data ['id'][]= $id;
-	           $data['title'][]= $title;
-		$data ['link'][]= $link;
-	    }
-	    	$stmt->close();
 	}
- 
-		$db_connect->close();
+	
+	require_once("./pdo.php");
 
+	try {
+		$stmt = $pdo->prepare("SELECT `id`,`title`,`link` FROM  `article` WHERE `uid` =? ORDER BY `create_time` DESC");
+		$stmt->bindParam(1, $_SESSION['userid']);
+		 $stmt->execute();
+		$arr =  array();
+		 while ($row = $stmt->fetch()) {
+		   	$arr[] = $row;
+		  }
+		$pdo = null;
+	} catch (PDOException $e) {		   
+		    die();
+	}
+	$count = count($arr);	   
 ?>
 
 <!DOCTYPE html>
@@ -42,73 +27,73 @@ if (!isset($_SESSION['pwd'])){
 <head>
 	<title>OurBlog</title>
 </head>
-<body><p>
-<div style="margin-top:200px;margin-left:400px">
-<table>
+<body>
+	<div style="margin-top:200px;margin-left:400px">
+		<table>
+			<tr>
+			<td><p><h1>OurBlog</h1></p></td>
+			<td style="width: 100px"></td>
+			<td><a href="" style="text-decoration:none; "><p>BlogManager</p></a></td>
+			<td style="width: 100px"></td>
+			<td><a href="./edit_html.php" style="text-decoration:none; "><p>WriteBlog</p></a></td>
+			</tr>
+		</table>
 
-		<tr>
-        <td><p><h1>OurBlog</h1></p></td>
-        <td style="width: 100px"></td>
-        <td><a href="" style="text-decoration:none; "><p>BlogManager</p></a></td>
-        <td style="width: 100px"></td>
-        <td><a href="./edit_html.php" style="text-decoration:none; "><p>WriteBlog</p></a></td>
-    </tr>
-</table>
+		<hr>
 
-<hr>
+	<div style="text-align: center;margin-top: 50px">
+		<table>
+	   		<?php     for ($i=0 ;$i<$count;$i++) {   ?>
+			<tr>
+	       		 <td width="400px" align="left"><?php  echo $arr[$i]['title']; ?></td>
+	        		<td width="100px"><a href="./exchange.php?id=<?php echo $arr[$i]['id']; ?>" style="text-decoration:none; ">edit</a></td>
+	        		<td width="100px"><a href="javascript:del('<?php echo $arr[$i]['id']; ?>')" style="text-decoration:none; ">del</a></td>
+	    		</tr>
+			<?php	}     ?>      
+		</table>
+			<script type="text/javascript">
+				function del(id){ 
+					var check = confirm('R U sure to del it?');
 
-<div style="text-align: center;margin-top: 50px">
-<table>
-   <?php 
-		for ($i=0 ;$i<$row;$i++) {   ?>
-		<tr>
-        <td width="400px" align="left"><?php echo $data['title'][$i]; ?></td>
-        <td width="100px"><a href="./exchange.php?id=<?php echo $data['id'][$i]; ?>" style="text-decoration:none; ">edit</a></td>
-        <td width="100px"><a href="javascript:del('<?php echo $data['id'][$i]; ?>')" style="text-decoration:none; ">del</a></td>
-    </tr>
-<?php		}
+						if(check){
+							var postData = {"id":id}, 
+							postData = (function(obj){ 
+							var str = "";
+							for(var prop in obj){
+							        str += prop + "=" + obj[prop] + "&"
+							 }
+							str=str.substring(0,str.length-1);
+							    return str;
+							})(postData);
 
+							var xhr = new XMLHttpRequest();
+							xhr.open("POST", "./del.php", true);
+							xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 
- ?>
-      
-</table>
-<script type="text/javascript">
-	function del(id){ 
-			var check = confirm('R U sure to del it?');
-			if(check){
-			var postData = {"id":id}, 
-			postData = (function(obj){ 
-		    var str = "";
-		    for(var prop in obj){
-		        str += prop + "=" + obj[prop] + "&"
-		    }
-		    str=str.substring(0,str.length-1);
-		    return str;
-			})(postData);
-			var xhr = new XMLHttpRequest();
-			xhr.open("POST", "./del.php", true);
-			xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-			xhr.onreadystatechange = function(){
-		    var XMLHttpReq = xhr;
-		    if (XMLHttpReq.readyState == 4 && XMLHttpReq.status == 200) {       
-		    var text = XMLHttpReq.responseText;
-		     if(text == true){
-		         	alert('delete success!');
-		         	location.reload();
-		      }
-		      else{
-		         	alert('delete error!');
-		      	}     
-		    }
-			};
-		xhr.send(postData);
-	}else{
-		return false;
-	}
-	}
-</script>
-</div>
-</div>
+							xhr.onreadystatechange = function(){
+							var XMLHttpReq = xhr;
+								if (XMLHttpReq.readyState == 4 && XMLHttpReq.status == 200) {       
+									var text = XMLHttpReq.responseText;
+									if(text == true){
+							         			alert('delete success!');
+							         			location.reload();
+							      		}
+							      		else{
+							         			alert('delete error!');
+							      		}     
+								}
+							};
+
+							xhr.send(postData);
+						}
+
+						else{
+							return false;
+						}
+				}
+			</script>
+	</div>
+	</div>
 
 </body>
 </html>
